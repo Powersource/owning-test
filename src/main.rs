2877deac4a0227ca6746879fa8e8a27dd909814e
Jacob Karlsson <jacob.karlsson95@gmail.com>
2016-12-08 20:36:59 +0100

    Make string duplication work

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,8 @@ fn main() {
 
 fn do_stuff(my_string: &String) -> String {
     println!("2: {}", my_string);
-    let temp = my_string;
     let mut new_string = "".to_string();
-    new_string.push_str(temp);
-    new_string.push_str(temp);
+    new_string.push_str(my_string);
+    new_string.push_str(my_string);
     new_string
 }
